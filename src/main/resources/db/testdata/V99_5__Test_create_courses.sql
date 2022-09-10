a9c3c850fb84496e453a4ab9b845d325c2f3f6a4
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-10 12:08:24 +0300

    test: course

---
+++ src/main/resources/db/testdata/V99_5__Test_create_courses.sql
@@ -0,0 +1,5 @@
+insert into "course"
+(id, name, ref, credits, total_hours)
+values ('course1_id', 'PROG1', 'Algorithmic', 15, 20),
+       ('course2_id', 'PROG2', 'Oriented Object Programming', 14, 25),
+       ('course3_id', 'EL1P2', 'PWA / SPA', 13, 13);
---
+++ src/test/java/school/hei/haapi/integration/CourseIT.java
@@ -0,0 +1,131 @@
+package school.hei.haapi.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import school.hei.haapi.SentryConf;
+import school.hei.haapi.endpoint.rest.api.TeachingApi;
+import school.hei.haapi.endpoint.rest.client.ApiClient;
+import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.Course;
+import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
+import school.hei.haapi.integration.conf.AbstractContextInitializer;
+import school.hei.haapi.integration.conf.TestUtils;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static school.hei.haapi.integration.conf.TestUtils.COURSE1_ID;
+import static school.hei.haapi.integration.conf.TestUtils.COURSE2_ID;
+import static school.hei.haapi.integration.conf.TestUtils.COURSE3_ID;
+import static school.hei.haapi.integration.conf.TestUtils.GROUP1_ID;
+import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
+import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Testcontainers
+@ContextConfiguration(initializers = CourseIT.ContextInitializer.class)
+@AutoConfigureMockMvc
+class CourseIT {
+    @MockBean
+    private SentryConf sentryConf;
+    @MockBean
+    private CognitoComponent cognitoComponentMock;
+
+    private static ApiClient anApiClient(String token) {
+        return TestUtils.anApiClient(token, PaymentIT.ContextInitializer.SERVER_PORT);
+    }
+
+    static Course course1() {
+        return new Course()
+                .id(COURSE1_ID)
+                .name("Algorithmic")
+                .ref("PROG1")
+                .credits(15)
+                .totalHours(20);
+    }
+
+    static Course course2() {
+        return new Course()
+                .id(COURSE2_ID)
+                .name("Oriented Object Programming")
+                .ref("PROG2")
+                .credits(14)
+                .totalHours(25);
+    }
+
+    static Course course3() {
+        return new Course()
+                .id(COURSE3_ID)
+                .name("PWA / SPA")
+                .ref("EL1P2")
+                .credits(13)
+                .totalHours(24);
+    }
+
+    @BeforeEach
+    void setUp() {
+        setUpCognito(cognitoComponentMock);
+    }
+
+    @Test
+    void student_read_ok() throws ApiException {
+        ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+        TeachingApi api = new TeachingApi(student1Client);
+
+        Course actual1 = api.getCourseById(COURSE1_ID);
+        List<Course> actualCourses = api.getCourses();
+
+        assertEquals(course1(), actual1);
+        assertTrue(actualCourses.contains(course1()));
+        assertTrue(actualCourses.contains(course2()));
+    }
+
+    @Test
+    void manager_read_ok() throws ApiException {
+    }
+
+    @Test
+    void student_read_ko() {
+    }
+
+    @Test
+    void teacher_read_ko() {
+    }
+
+    @Test
+    void manager_write_ok() throws ApiException {
+    }
+
+    @Test
+    void teacher_write_ko() {
+    }
+
+    @Test
+    void student_write_ko() {
+    }
+
+    @Test
+    void manager_write_ko() throws ApiException {
+    }
+
+    @Test
+    void manager_write_with_some_bad_fields_ko() {
+    }
+
+    static class ContextInitializer extends AbstractContextInitializer {
+        public static final int SERVER_PORT = anAvailableRandomPort();
+
+        @Override
+        public int getServerPort() {
+            return SERVER_PORT;
+        }
+    }
+}
