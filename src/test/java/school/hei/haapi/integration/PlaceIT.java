9f5785433552efc7d7e0ea21474c004937b20181
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-11 19:22:09 +0300

    test: integration

--- src/test/java/school/hei/haapi/integration/PlaceIT.java
+++ src/test/java/school/hei/haapi/integration/PlaceIT.java
@@ -18,6 +18,7 @@ import school.hei.haapi.integration.conf.TestUtils;
 
 import java.util.List;
 
+import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -29,6 +30,7 @@ import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.TEACHER1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.assertThrowsForbiddenException;
+import static school.hei.haapi.integration.conf.TestUtils.isValidUUID;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -42,7 +44,7 @@ class PlaceIT {
     private CognitoComponent cognitoComponentMock;
 
     private static ApiClient anApiClient(String token) {
-        return TestUtils.anApiClient(token, PaymentIT.ContextInitializer.SERVER_PORT);
+        return TestUtils.anApiClient(token, PlaceIT.ContextInitializer.SERVER_PORT);
     }
 
     static Place place1() {
@@ -55,6 +57,12 @@ class PlaceIT {
         return new Place()
                 .id(PLACE2_ID)
                 .label("AF Andavamamba");
+    }
+
+    public static Place someCreatablePlace() {
+        Place place = new Place();
+        place.setLabel("label-"+ randomUUID());
+        return place;
     }
 
     @BeforeEach
@@ -119,23 +127,72 @@ class PlaceIT {
 
     @Test
     void manager_write_ok() throws ApiException {
+        ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+        Place toCreate3 = someCreatablePlace();
+        Place toCreate4 = someCreatablePlace();
 
-    }
+        AttendingApi api = new AttendingApi(manager1Client);
+        List<Place> created = api.saveOrUpdatePlaces(List.of(toCreate3, toCreate4));
 
-    @Test
-    void teacher_write_ko() {
+        assertEquals(2, created.size());
+
+        // created3
+        Place created3 = created.get(0);
+        assertTrue(isValidUUID(created3.getId()));
+        toCreate3.setId(created3.getId());
+        assertEquals(created3, toCreate3);
+        // created4
+        Place created4 = created.get(0);
+        assertTrue(isValidUUID(created4.getId()));
+        toCreate4.setId(created4.getId());
+        assertEquals(created4, toCreate3);
     }
 
     @Test
     void student_write_ko() {
+        ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
+        AttendingApi api = new AttendingApi(student1Client);
+
+        assertThrowsForbiddenException(() -> api.saveOrUpdatePlaces(List.of()));
     }
 
     @Test
-    void manager_write_ko() throws ApiException {
+    void teacher_write_ok() throws ApiException {
+        ApiClient teacher1Client = anApiClient(TEACHER1_TOKEN);
+        AttendingApi api = new AttendingApi(teacher1Client);
+
+        List<Place> toUpdate = api.saveOrUpdatePlaces(List.of(
+                someCreatablePlace(),
+                someCreatablePlace()));
+        Place toUpdate0 = toUpdate.get(0);
+        toUpdate0.setLabel("A new name zero");
+        Place toUpdate1 = toUpdate.get(1);
+        toUpdate1.setLabel("A new name one");
+
+        List<Place> updated = api.saveOrUpdatePlaces(toUpdate);
+
+        assertEquals(2, updated.size());
+        assertTrue(updated.contains(toUpdate0));
+        assertTrue(updated.contains(toUpdate1));
     }
 
     @Test
-    void manager_write_with_some_bad_fields_ko() {
+    void manager_write_update_ok() throws ApiException {
+        ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+        AttendingApi api = new AttendingApi(manager1Client);
+        List<Place> toUpdate = api.saveOrUpdatePlaces(List.of(
+                someCreatablePlace(),
+                someCreatablePlace()));
+        Place toUpdate0 = toUpdate.get(0);
+        toUpdate0.setLabel("Ambohimanarina");
+        Place toUpdate1 = toUpdate.get(1);
+        toUpdate1.setLabel("Analakely");
+
+        List<Place> updated = api.saveOrUpdatePlaces(toUpdate);
+
+        assertEquals(2, updated.size());
+        assertTrue(updated.contains(toUpdate0));
+        assertTrue(updated.contains(toUpdate1));
     }
 
     static class ContextInitializer extends AbstractContextInitializer {
