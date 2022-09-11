3daea37140f5210bad3d41a8deeb64c549ee880d
Yume <96466759+YumeT023@users.noreply.github.com>
2022-09-11 19:21:14 +0300

    chore: add validator

--- src/main/java/school/hei/haapi/model/validator/PlaceValidator.java
+++ src/main/java/school/hei/haapi/model/validator/PlaceValidator.java
@@ -1,11 +1,13 @@
 package school.hei.haapi.model.validator;
 
+import org.springframework.stereotype.Component;
 import school.hei.haapi.model.Place;
 import school.hei.haapi.model.exception.BadRequestException;
 
 import java.util.List;
 import java.util.function.Consumer;
 
+@Component
 public class PlaceValidator implements Consumer<Place> {
     @Override
     public void accept(Place tocheck) {
